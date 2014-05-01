c02a7d1ef4476da23d0e8a1980023b96604ce90b
Justin Woodcock <jwoodcock@undergroundelephant.com>
2014-04-30 22:38:52 -0700

    add fontello icon font

--- app/js/info/controller.info.js
+++ app/js/info/controller.info.js
@@ -4,6 +4,11 @@ uiBase.controller('InfoController', ['$scope',
     function($scope) {
         $scope.version = '0.0.1';
         $scope.todos = [{
+            item: 'add restangular example',
+            description: 'a basic implementation that someone could pick up and run with.',
+            githubUrl: '#',
+            completed: false
+        }, {
             item: 'add unit test',
             description: 'just one basic test for an example, we\'ll check the version number.',
             githubUrl: '#',
@@ -12,7 +17,7 @@ uiBase.controller('InfoController', ['$scope',
             item: 'add fontello icon font',
             description: 'just a few basic font icons along with the config for future additions.',
             githubUrl: 'https://elephanthub.net/ui/ui-base/issues/1',
-            completed: false
+            completed: true
         }, {
             item: 'add list of thirdparty libraries used',
             description: 'simple list added to the main view that gives the title and link to all external assets.',
