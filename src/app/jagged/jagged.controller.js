c5c897fae7753b5f043f3acf0153e8e33669a432
Justin Woodcock <justin@justinkwoodcock.local>
2015-04-14 11:28:10 -0700

    add .toParams to the .state object

--- src/app/jagged/jagged.controller.js
+++ src/app/jagged/jagged.controller.js
@@ -5,6 +5,7 @@ angular.module('jagged').controller('JaggedController', ['$scope', '$rootScope',
         // This function fires on every stateChangeStart event and sets the $scope.state variable to the current state.
         $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
             $scope.state = toState;
+            $scope.state.params = toParams;
         });
         // set the sessionData variable for localStorage data to be used throughout the app.
         $scope.sessionData = JaggedFactory.getStorage();
